d58d6ebd03dd31777c6f933187747131d748bec6
Yurii Skrynnykov <truef1s7@gmail.com>
2019-02-12 22:57:49 +0200

    Implement PoC.

---
+++ examples/src/index.md
@@ -0,0 +1,27 @@
+# <%= header %>
+
+## Sub title
+
+Hello out there testing my ssg.
+
+Blas bla bla:
+* adsafd
+* fgdsgfds [test](https://f1sty.org)
+* fsdfsd
+
+<%= for x <- 1..4 do %>
+<%= x %>. item here
+<% end %>
+
+- [x] gfdsgsfd
+- [ ] fgdga
+- [x] gfdggrethgtrh
+
+```
+tegind
+fdsg ffdsf
+```
+
+*here* we go `agfai` **fsdgsdfgfd** hohoho!
+
+<%= footer %>
--- lib/jexyll.ex
+++ lib/jexyll.ex
@@ -1,12 +1,29 @@
 defmodule Jexyll do
   @moduledoc false
 
+  @default_type :html
+
   def start(config_file \\ "jexyll.json") do
     config = config(config_file)
 
-    config
-    |> get_in(~w/site src_dir/)
-    |> File.ls!
+    src_dir = get_in(config, ~w/site src_dir/)
+    build_dir = get_in(config, ~w/site build_dir/)
+
+    vars =
+      get_in(config, ~w/site env/)
+      |> Enum.reduce(%{}, &Map.merge(&2, &1))
+      |> Enum.map(fn {k, v} -> {String.to_atom(k), v} end)
+
+    files =
+      src_dir
+      |> glob_type()
+      |> Enum.map(&parse_filename/1)
+      |> Enum.reduce(%{}, fn {k, v}, acc -> Map.update(acc, k, [v], &[v | &1]) end)
+
+    files.html
+    |> Stream.map(fn file -> {String.replace(file, src_dir, build_dir) <> ".html", File.read!(file <> ".md") |> EEx.eval_string(vars)} end)
+    |> Stream.map(fn {file, parsed} -> {file, Earmark.as_html!(parsed)} end)
+    |> Enum.each(&write_parsed/1)
   end
 
   defp config(config_file) do
@@ -14,5 +31,32 @@ defmodule Jexyll do
     |> Path.absname()
     |> File.read!()
     |> Jason.decode!()
+  end
+
+  defp parse_filename(filename, type \\ :md) do
+    basepath =
+      case type do
+        :md -> String.trim_trailing(filename, ".md")
+        :eex -> String.trim_trailing(filename, ".eex")
+        _ -> raise("type #{type} not implemented yet")
+      end
+
+    {@default_type, basepath}
+  end
+
+  defp glob_type(src_dir, type \\ :md) do
+    src_dir
+    |> Path.join("**/*.#{type}")
+    |> Path.wildcard()
+  end
+
+  defp write_parsed({filename, content}) do
+    filename
+    |> Path.dirname()
+    |> File.mkdir_p!()
+
+    filename
+    |> File.open!([:write])
+    |> IO.write(content)
   end
 end
